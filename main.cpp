2f322b80d5fdbc3f6c6c67d8bc47d34a302487ab
calebchase <calebchase0308@gmail.com>
2023-04-03 22:34:28 -0400

    DCT

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <math.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
@@ -69,10 +70,78 @@ void WriteYCbCrArray(int width, int height, int channels, vector<vector<PixelYCb
     stbi_write_bmp("output.bmp", width, height, 3, outData);
 }
 
+float DCTCosVal(int firstIndex, int secondIndex)
+{
+    return std::cos((3.14159262 / (8.0)) * (firstIndex + 0.5) * secondIndex);
+}
+
+int getPixelVal(PixelYCbCr *pixel, int type)
+{
+    if (type == 0)
+        return pixel->Y;
+    if (type == 1)
+        return pixel->Cb;
+    if (type == 2)
+        return pixel->Cr;
+    return -1;
+}
+
+// Might have bugs
+void DCT(vector<vector<PixelYCbCr>> *pixelArray, int rowIndex, int colIndex, int type)
+{
+    float ci = 0;
+    float cj = 0;
+    float sum = 0;
+    float dctTemp[8][8];
+
+    (*pixelArray)[0 + 0][0 + colIndex].Y = 255;
+    cout << (*pixelArray)[0][0].Y;
+
+    // used for testing
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     for (int j = 0; j < 8; j++)
+    //     {
+    //         (*pixelArray)[i][j].Y = 255;
+    //     }
+    // }
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            ci = i == 0 ? 1 / std::sqrt(8.0) : std::sqrt(2) / std::sqrt(8);
+            cj = j == 0 ? 1 / std::sqrt(8.0) : std::sqrt(2) / std::sqrt(8);
+
+            sum = 0;
+            for (int k = 0; k < 8; k++)
+            {
+                for (int l = 0; l < 8; l++)
+                {
+                    cout << getPixelVal(&((*pixelArray)[i + rowIndex][j + colIndex]), type);
+                    sum += getPixelVal(&((*pixelArray)[i + rowIndex][j + colIndex]), type) * DCTCosVal(k, i) * DCTCosVal(l, j);
+                }
+                cout << "\n";
+            }
+            dctTemp[i][j] = ci * cj * sum;
+        }
+    }
+
+    // used for testing
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     for (int j = 0; j < 8; j++)
+    //     {
+    //         cout << (float)dctTemp[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+}
+
 int main()
 {
     int width, height, channels;
-    unsigned char *img = stbi_load("lenna.png", &width, &height, &channels, 3);
+    unsigned char *img = stbi_load("tree.jpg", &width, &height, &channels, 3);
 
     if (img == NULL)
     {
@@ -81,8 +150,10 @@ int main()
     }
 
     std::cout << "Loaded image with a width of " << width << "px, a height of " << height << " px and  " << channels << " channels\n";
-    vector<vector<PixelYCbCr>> outVector(height, vector<PixelYCbCr>(width));
+    vector<vector<PixelYCbCr>> pixelArray(height, vector<PixelYCbCr>(width));
 
-    CreateYCbCrArray(img, &outVector, 3);
-    WriteYCbCrArray(width, height, 3, &outVector);
+    CreateYCbCrArray(img, &pixelArray, 3);
+    DCT(&pixelArray, 0, 0, 0);
+
+    // WriteYCbCrArray(width, height, 3, &pixelArray);
 }
